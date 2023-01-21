3c8b328324c467d4de61a084968923b446687399
pickfix <Prikhodko.B.T@gmail.com>
2023-01-21 17:12:35 +0200

    incorect app

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import Form from './components/Form/Form';
+import Section from './components/Section/Section';
+import Contacts from './components/Contacts/Contacts';
+import Filter from './components/Filter/Filter';
+import { v4 as uuidv4 } from 'uuid';
+
+class App extends Component {
+  state = {
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: '',
+  };
+
+  formSubmitHandler = data => {
+    const { contacts } = this.state;
+    const isContactValid = this.validateContact(data, contacts);
+
+    if (isContactValid) {
+      data.id = uuidv4();
+      this.setState(({ contacts }) => ({
+        contacts: [data, ...contacts],
+      }));
+    }
+  };
+
+  validateContact = (data, contacts) => {
+    if (contacts.some(({ name }) => name === data.name)) {
+      alert(`${data.name} is already in contacts.`);
+      return false;
+    } else return true;
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  handleSearch = e => {
+    this.setState({
+      filter: e.currentTarget.value,
+    });
+  };
+
+  getFiltredContacts() {
+    const { contacts, filter } = this.state;
+    const lowerCaseFilter = filter.toLowerCase();
+    return contacts.filter(person =>
+      person.name.toLowerCase().includes(lowerCaseFilter),
+    );
+  }
+
+  render() {
+    const { filter } = this.state;
+    const filteredContacts = this.getFiltredContacts();
+
+    return (
+      <>
+        <Section title="Phonebook">
+          <Form onSubmit={this.formSubmitHandler} />
+        </Section>
+
+        <Section title="Contacts">
+          <Filter value={filter} onChange={this.handleSearch} />
+          <Contacts
+            contacts={filteredContacts}
+            onDeleteBtnClick={this.deleteContact}
+          />
+        </Section>
+      </>
+    );
+  }
+}
+
+export default App;
